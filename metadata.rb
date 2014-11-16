cdbbe19fd802c062c7695dd452af21edb8a4048a
Tim Smith <tim@cozy.co>
2014-11-15 18:08:39 -0800

    Release 1.0.3 to fix bad erb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tsmith@limelight.com'
 license          'Apache 2.0'
 description      'Installs/Configures sensor polling using lm-sensors or openipmi on Linux systems'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.2'
+version          '1.0.3'
 
 %w(debian ubuntu centos redhat oracle scientific).each do |os|
   supports os
