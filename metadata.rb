02c645a046586542f5144c537211b5ce25da09a1
Tim Smith <tim@cozy.co>
2014-11-15 18:02:29 -0800

    Fix lack of chips failing the chef run
    Also if no description is provided just use the ID

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tsmith@limelight.com'
 license          'Apache 2.0'
 description      'Installs/Configures sensor polling using lm-sensors or openipmi on Linux systems'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.0.2'
 
 %w(debian ubuntu centos redhat oracle scientific).each do |os|
   supports os
