3d0e9c6c9364cbff7b595f6aa18260fce5f681c7
Tim Smith <tim@cozy.co>
2015-08-08 02:09:36 -0700

    Fix the name of the the cookbook to test

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 describe 'default installation' do
   let(:runner) { ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '12.04') }
-  let(:chef_run) { runner.converge('smokeping::default') }
+  let(:chef_run) { runner.converge('sensors::default') }
 end
