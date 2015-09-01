59c1802701cbe1fc288b6a997c7752f41d87db39
Tim Smith <tsmith84@gmail.com>
2015-08-31 23:13:04 -0700

    Add fedora to the metadata

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,11 @@ maintainer 'Tim Smith'
 maintainer_email 'tsmith84@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures sensor polling using lm-sensors or openipmi on Linux systems'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.1.0'
 
-%w(debian ubuntu centos redhat oracle scientific).each do |os|
+%w(debian ubuntu centos redhat fedora oracle scientific).each do |os|
   supports os
 end
+
+source_url 'https://github.com/tas50/sensors' if respond_to?(:source_url)
+issues_url 'https://github.com/tas50/sensors/issues' if respond_to?(:issues_url)
