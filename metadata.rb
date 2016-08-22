01cd767c2d7b5289b6485eaca50d8c0ebc0a402c
Tim Smith <tsmith@chef.io>
2016-08-22 09:56:14 -0700

    Testing updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer 'Tim Smith'
 maintainer_email 'tsmith84@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures sensor polling using lm-sensors or openipmi on Linux systems'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.1.4'
 
 %w(debian ubuntu centos redhat fedora oracle scientific).each do |os|
@@ -11,3 +12,5 @@ end
 
 source_url 'https://github.com/tas50/chef-sensors' if respond_to?(:source_url)
 issues_url 'https://github.com/tas50/chef-sensors/issues' if respond_to?(:issues_url)
+
+chef_version '>= 11.0' if respond_to?(:chef_version)
