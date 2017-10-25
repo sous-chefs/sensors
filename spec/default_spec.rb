ea1a5d7aac2f27662aed30ed22295ab819a88374
Tim Smith <tsmith@chef.io>
2017-10-25 10:01:47 -0700

    Update specs to sorta do something
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -3,4 +3,8 @@ require 'spec_helper'
 describe 'default installation' do
   let(:runner) { ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '12.04') }
   let(:chef_run) { runner.converge('sensors::default') }
+
+  it 'converges successfully' do
+    expect { :chef_run }.to_not raise_error
+  end
 end
