0ccdb465522d7ad9c307c6baa2b4f8ebf1e5df0b
Marcos Oliveira <marcosvm@gmail.com>
2019-10-10 10:27:23 -0700

    update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
 license 'Apache-2.0'
 description 'Installs/Configures sensor polling using lm-sensors or openipmi on Linux systems'
-
 version '1.1.6'
 
 %w(debian ubuntu centos redhat fedora oracle scientific).each do |os|
